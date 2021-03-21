7752e5ddef8aee88be44e51f6f4a7bf1314c2750
monzoor-rm <monzoor.morshed@raremark.com>
2021-03-22 02:54:36 +0600

    asdasd

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,7 @@ var cp = require("child_process");
 var path = require("path");
 
 const args = [];
-// cp.spawn("bash", [path.join(__dirname, "react.sh")].concat(args), {
-//   stdio: "inherit",
-// });
-console.log("+++++");
+cp.spawn("bash", [path.join(__dirname, "react.sh")].concat(args), {
+  stdio: "inherit",
+});
+console.log("Done");
