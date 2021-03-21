77e07fb1dce4aae41591ffd7ea0134c7c9239a4c
monzoor-rm <monzoor.morshed@raremark.com>
2021-03-22 03:06:44 +0600

    testing

---
+++ bin/init.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+
+var cp = require("child_process");
+var path = require("path");
+
+const args = [];
+cp.spawn("bash", [path.join(__dirname, "init.sh")].concat(args), {
+  stdio: "inherit",
+});
+console.log("Done wow");
