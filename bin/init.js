fc2b7d70febbae0bebc8d5269defe77746124a09
monzoor-rm <monzoor.morshed@raremark.com>
2021-03-22 13:44:19 +0600

    Update

--- bin/init.js
+++ bin/init.js
@@ -1,11 +1,41 @@
 #! /usr/bin/env node
 
-const cp = require('child_process');
+// const cp = require('child_process');
 const path = require('path');
 const chalk = require('chalk');
 
 const args = [];
-cp.spawn('bash', [path.join(__dirname, 'init.sh')].concat(args), {
-  stdio: 'inherit',
-});
-console.log(chalk.green('Your template is ready.\nHappy working!!!'));
+
+async function spawnChild() {
+  const { spawn } = require('child_process');
+  const child = spawn('bash', [path.join(__dirname, 'init.sh')].concat(args));
+
+  let data = '';
+  for await (const chunk of child.stdout) {
+    console.log(chalk.yellow('stdout chunk: ' + chunk));
+    data += chunk;
+  }
+  let error = '';
+  for await (const chunk of child.stderr) {
+    console.error(chalk.red('stderr chunk: ' + chunk));
+    error += chunk;
+  }
+  const exitCode = await new Promise((resolve, reject) => {
+    child.on('close', resolve);
+  });
+
+  if (exitCode) {
+    throw new Error(chalk.red(`subprocess error exit ${exitCode}, ${error}`));
+  }
+  return data;
+}
+
+spawnChild().then(
+  (data) => {
+    console.log('async result:\n' + data);
+    console.log(chalk.green('Your template is ready.\nHappy working!!!'));
+  },
+  (err) => {
+    console.error(chalk.red('async error:\n' + err));
+  },
+);
