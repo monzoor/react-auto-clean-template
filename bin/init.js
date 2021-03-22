7bc50a6c12888ad2c13e06e99ef63eb4a7da9ee0
monzoor-rm <monzoor.morshed@raremark.com>
2021-03-23 03:32:24 +0600

    Update

--- bin/init.js
+++ bin/init.js
@@ -2,31 +2,21 @@
 
 const path = require('path');
 const chalk = require('chalk');
-const cliSpinners = require('cli-spinners');
-const logUpdate = require('log-update');
-const args = [];
-const spinner = cliSpinners['simpleDots'];
+const { spawn } = require('child_process');
 
+const args = [];
 async function spawnChild() {
-  const { spawn } = require('child_process');
   const child = spawn('bash', [path.join(__dirname, 'init.sh')].concat(args));
 
   let data = '';
 
   for await (const chunk of child.stdout) {
     console.log(`${chalk.green('Process: ')} ${chunk}`);
-    // let i = 0;
-
-    // setInterval(() => {
-    //   const { frames } = spinner;
-    //   logUpdate(chalk.yellow('working ') + frames[(i = ++i % frames.length)]);
-    // }, spinner.interval);
-
     data += chunk;
   }
   let error = '';
   for await (const chunk of child.stderr) {
-    console.error(+chunk);
+    console.error(`${chunk}`);
     error += chunk;
   }
   const exitCode = await new Promise((resolve, reject) => {
