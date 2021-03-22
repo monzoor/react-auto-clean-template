b211b96be131b1f7c2623211c10fecb353b32473
monzoor-rm <monzoor.morshed@raremark.com>
2021-03-22 15:18:37 +0600

    Update

--- bin/index.js
+++ bin/index.js
@@ -3,8 +3,23 @@
 const cp = require('child_process');
 const path = require('path');
 const chalk = require('chalk');
+var fs = require('fs');
 
 console.log(chalk.keyword('orange')('Initializing...'));
+
+const item = {
+  scripts: {
+    start: 'node node_modules/react-auto-clean-template/bin/init.js',
+  },
+};
+
+var json = JSON.stringify(item);
+fs.writeFile('./package.json', json, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
 
 const args = [];
 cp.spawn('bash', [path.join(__dirname, 'react.sh')].concat(args), {
--- bin/react.sh
+++ bin/react.sh
@@ -2,4 +2,4 @@
 
 npm i react-auto-clean-template
 
-node node_modules/react-auto-clean-template/bin/init.js
+yarn start
